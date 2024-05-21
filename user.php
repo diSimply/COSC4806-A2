7d32c8a9215e7f003208ca21e24043990032f634
Diana C <165112039+diSimply@users.noreply.github.com>
2024-05-21 22:31:20 +0000

    Added getUserByUsername, getUserByUsernameAndPassword, createUser methods

--- user.php
+++ user.php
@@ -13,6 +13,29 @@ class User {
     return $rows;
   }
 
+  public function getUserByUsernamePassword($username, $password) {
+    $db = db_connect();
+    $statement = $db->prepare("SELECT * FROM users WHERE username = :username AND password = :password");
+    $statement->bindParam(':username', $username);
+    $statement->bindParam(':password', $password);
+    $statement->execute();
+    $row = $statement->fetch(PDO::FETCH_ASSOC);
+
+    return $row;
+  }
+
+  public function getUserByUsername($username) {
+    $db = db_connect();
+    $statement = $db->prepare("SELECT * FROM users WHERE username = :username");
+    $statement->bindParam(':username', $username);
+    $statement->execute();
+    $row = $statement->fetch(PDO::FETCH_ASSOC);
+
+    return $row;
+  }
+
+  
+
   // create new user
   public function createUser($username, $password){
     $db = db_connect();
