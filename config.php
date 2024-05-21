0ff2839f8437b2e1dec7b04481177059c19d6447
Diana C <165112039+diSimply@users.noreply.github.com>
2024-05-21 21:26:15 +0000

    Added config file

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+
+define('DB_HOST', 'gol.h.filess.io');
+define('DB_USER', 'COSC4806_seemsupbet');
+define('DB_PASS', $_ENV['DB_PASS']);
+define('DB_NAME', 'COSC4806_seemsupbet');
+define('DB_PORT', '3305');
+
+?>
