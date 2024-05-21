20f1630bd2438a5cad43e820838f3d4758bb1bf6
Diana C <165112039+diSimply@users.noreply.github.com>
2024-05-21 22:33:58 +0000

    Updated index page

--- index.php
+++ index.php
@@ -1,9 +1,32 @@
 <?php
-
-require_once('user.php');
-
-$user = new User();
-$user_rows = $user->getAllUsers();
-
-print_r($user_rows);
+  session_start();
+  if (isset($_SESSION['authenticated'])) {
+    $username = $_SESSION['username'];
+    $authenticated = $_SESSION['authenticated'];
+    if (!$authenticated) {
+      // redirect to login page when not authenticated
+      header('location: /login.php');
+    }
+  } else {
+    // redirect to login page when not authenticated
+    header('location: /login.php');
+  }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+  <h1>Assignment 2 Home</h1>
+  <p>
+   <div>Welcome <strong><?php echo $username ?></strong>!</div>
+   <div>Today is <i><?php echo date("M d, Y")?></i>.</div> 
+   <div>You are in the home page.</div>
+   <a href="logout.php">Logout</a>
+  </p>
+</body>
+</html>
