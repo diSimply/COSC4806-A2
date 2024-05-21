dcb52af2578badebc57b5c7611496de7a7d41ca5
Diana C <165112039+diSimply@users.noreply.github.com>
2024-05-21 22:33:20 +0000

    Added login and validate functionality

---
+++ validate.php
@@ -0,0 +1,33 @@
+<?php
+
+  require_once('user.php');
+  $user = new User();
+
+  session_start();
+
+  
+
+  $username = $_REQUEST['username'];
+  $password = $_REQUEST['password'];
+
+  $user_row = $user->getUserByUsernamePassword($username, $password);
+
+
+  if ($user_row) {
+    $_SESSION['authenticated'] = true;
+    $_SESSION['failed_attempts'] = 0;
+    $_SESSION['username'] = $username;
+    header('location: /');
+  } else {
+    // check the session variable
+    if (isset($_SESSION['failed_attempts'])) {
+      // session variable already exists, update it
+      $_SESSION['failed_attempts'] = $_SESSION['failed_attempts'] + 1;
+    } else {
+      // session variable does not exist, initialize it
+       $_SESSION['failed_attempts'] = 1;
+    }
+    header('location: /login.php?error=invalid_credentials');
+  }
+
+?>
